48feb2c989440651e47eb4cfa06dba6c059d7d64
George Avsadzhanishvili <george.avsa@yandex.ru>
2023-04-20 23:58:33 +0300

    added dynamic bottom padding

--- src/UI/input/Datepicker.jsx
+++ src/UI/input/Datepicker.jsx
@@ -5,13 +5,14 @@ import generateMonth from "../../calendarFunction/generateMonth";
 import arrowLeft from "./../../images/arrowLeft.svg";
 import { HeadingM, HeadingS } from "../texts/Heading";
 
-function Datepicker({ label, ...props }) {
-    // use redux for this
-    const options = ["Lorem", "Ipsum"];
-    const [fieldValue, setFieldValue] = useState(options[0]);
-    // end
+function monthToText(mothDate) {
+    const fullDateText = mothDate.toString();
+    const dateMatch = fullDateText.match(/\w+\s(\w+)\s\d+\s(\d+)/);
+    return `${dateMatch[1]} ${dateMatch[2]}`;
+}
 
-    const [dropdown, setDropdown] = useState(true);
+function Datepicker({ label, ...props }) {
+    const [dropdown, setDropdown] = useState(false);
     const fieldRef = useRef(null);
 
     useEffect(() => {
@@ -29,45 +30,51 @@ function Datepicker({ label, ...props }) {
         setDropdown(true);
     }
 
-    function selectValue(e) {
-        const value = e.target.innerText;
-        if (!!value) {
-            setFieldValue(value);
-            setDropdown(false);
-        }
-    }
+    const calendarRef = useRef(null);
+
+    const monthDate = new Date(2023, 1, 1);
+
+    const [monthText, setMonthText] = useState("");
+
+    useEffect(() => {
+        setMonthText(monthToText(monthDate));
+        const height = calendarRef.current.getBoundingClientRect().height;
+        calendarRef.current.style.bottom = `-${Number.parseInt(height) + 10}px`;
+    }, [monthDate]);
 
     return (
         <div className='field'>
-            <div className='dropdowns'>
-                <div className='calendar__controls'>
-                    <img src={arrowLeft} alt='' />
-                    <HeadingS>Aug 2021</HeadingS>
-                    <img src={arrowLeft} alt='arrowRight' />
+            {dropdown && (
+                <div className='dropdowns' ref={calendarRef}>
+                    <div className='calendar__controls'>
+                        <img src={arrowLeft} alt='' />
+                        <HeadingS>{monthText}</HeadingS>
+                        <img src={arrowLeft} alt='arrowRight' />
+                    </div>
+                    <div className='calendar__body'>
+                        {generateMonth(monthDate).map((date) => {
+                            return (
+                                <div
+                                    className='calendar__item'
+                                    style={
+                                        !date.clickable
+                                            ? { opacity: ".5" }
+                                            : null
+                                    }
+                                >
+                                    {date.date.getDate()}
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
-                <div className='calendar__body'>
-                    {generateMonth(new Date(2023, 0, 2)).map((date) => {
-                        return (
-                            <div
-                                className='calendar__item'
-                                style={
-                                    !date.clickable ? { opacity: ".5" } : null
-                                }
-                            >
-                                {date.date.getDate()}
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
+            )}
             <div className='field__label'>{label}</div>
             <div
                 className='field__input'
                 onClick={(e) => handleDropdown(e)}
                 ref={fieldRef}
-            >
-                {fieldValue}
-            </div>
+            ></div>
         </div>
     );
 }
